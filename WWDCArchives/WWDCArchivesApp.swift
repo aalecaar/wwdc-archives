22cd8c9b693f0a3cdbf85b442b12937d26631028
Alejandro Cardona <alejo.98k@gmail.com>
2024-05-24 13:15:58 -0500

    Initial Commit

---
+++ WWDCArchives/WWDCArchivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCArchivesApp.swift
+//  WWDCArchives
+//
+//  Created by xx on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WWDCArchivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
